610385225cad981f6a4020f7fff5e7b6d2779665
poolpal <zoo5871@naver.com>
2020-08-24 23:06:52 +0900

    2020-08-24

--- web_script/Web.jsx
+++ web_script/Web.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React,{useState,memo} from 'react';
+import {Form,Button,PageHeader,Input,  Row, Col} from 'antd'
 
-const Web =()=>{
+
+
+
+const Web =memo(()=>{
+
 
     return(
         <>
-            Hello
+        
+        <PageHeader
+        className="site-page-header"
+        title="Title"
+        subTitle="This is a subtitle"></PageHeader>
+        <Form>
+            <Row>
+                <Col span={12}>
+                    <Input value="check"></Input>
+                </Col>
+                <Col span={12}>
+                    <Button type="primary" htmlType="submit">check</Button>
+                </Col>
+            </Row>
+            
+            
+            
+        </Form>
+        
+
         </>
     );
 
-}
+});
 
 export default Web;
